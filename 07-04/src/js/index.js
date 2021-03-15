9ca873b9e38f0dc2381803075368842eab8fcbdf
JavaEdge <sss2855845850@gmail.com>
2021-03-15 16:29:43 +0800

    将评论 item 项抽取成一个 function

--- 07-04/src/js/index.js
+++ 07-04/src/js/index.js
@@ -62,11 +62,38 @@ const me = {
     gender: 'boy'
 }
 
+function CmtItem(props) {
+    return <div key={props.user}>
+        <h1>评论人：{props.user}</h1>
+        <p>评论内容：{props.content}</p>
+    </div>
+}
+
+class CmtList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            CommentList: [
+                {id: 1, user: 'sss', content: 'java'},
+                {id: 2, user: 'java', content: 'java'},
+                {id: 3, user: 'edge', content: 'java'},
+                {id: 4, user: 'nb', content: 'java'},
+            ]
+        }
+    }
+
+    render() {
+        return <div>
+            <h1> 这是评论列表组件 </h1>
+            {this.state.CommentList.map(item => <CmtItem {...item}></CmtItem>)}
+        </div>
+    }
+}
+
 ReactDOM.render(<div>
     {/*这里的 Index 标签，就是 Index 类的一个实例对象*/}
     {/*这里就是外界传来的 me，可用 props 访问*/}
-    <Index {...me}>
-    </Index>
+    <CmtList>
+
+    </CmtList>
 </div>, document.getElementById('example'));
-// ReactDOM.render(
-// 	<Index/>, document.getElementById('example'));
